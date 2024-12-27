b66aaf40bcace673bf666820e5afc42f60e8b515
Fatemeh HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2024-12-28 00:31:43 +0330

    finish Coding HTML/CSS

--- asset/script.js
+++ asset/script.js
@@ -1,12 +1,1 @@
 let $=document
-let foodCart=[
-    {  id: 1,name: "Waffel",price:5.55},
-    {  id: 2,name: "Creme Brulee",price:5.00},
-    {  id: 3,name: "Macaron",price:1.00},
-    {  id: 4,name: "Tiramisu",price:3.05},
-    {  id: 5,name: "Baklava",price:4.32},
-    {  id: 6,name: "Pie",price:5.02},
-    {  id: 7,name: "Cake",price:5.00},
-    {  id: 8,name: "Brownie",price:5.22},
-    {  id: 9,name: "Panna Cotta",price:5.3}
-]
