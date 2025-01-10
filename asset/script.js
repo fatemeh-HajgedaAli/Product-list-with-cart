056d9dbda71090f798dbf1fbb485328754603d94
Fatemeh HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2025-01-10 15:54:11 +0330

    coding with js

--- asset/script.js
+++ asset/script.js
@@ -1,1 +1,12 @@
 let $=document
+let cartBtn=$.querySelectorAll('.cartBtn')
+let cartaddRemove=$.querySelectorAll('.cartaddRemove')
+
+//add food to cart by click on it ...
+cartBtn.forEach(function(button){
+let count=0;
+button.addEventListener('click' , function(){
+    count++
+console.log(`cartBtn: ${count}`)
+});
+});
