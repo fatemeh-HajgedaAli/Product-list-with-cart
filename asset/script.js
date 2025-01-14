2772bab01c9a07c691baa681e6069a58bccb413c
Fatemeh HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2025-01-14 22:49:47 +0330

    js

--- asset/script.js
+++ asset/script.js
@@ -3,42 +3,7 @@ let $=document
 let cart = []; // Array to hold the items added to the cart.
 let cartBtn = $.querySelectorAll('.cartBtn'); // Select all buttons related to the cart functionality.
 let buyItem=$.querySelector('.buyItem');
-/*
-cartBtn.forEach((button)=>
 
-button.addEventListener('click',function(button){
-const foodId=button.dataset.id;
-const foodName=foodName.dataset.name;
-const foodPrice=price.dataset.price;
-
-const isInCart = cart.some((item) => item.name === foodName); // Check if the item is already in the cart.
-const cartaddRemove = button.nextElementSibling; // Select the next sibling element (e.g., for showing/hiding add/remove options).
-
-if (isInCart) {
-    // If the item is already in the cart, remove it.
-    cart = cart.filter((item) => item.name !== foodName);
-    cartaddRemove.style.display = 'none'; // Hide the add/remove button.
-} else {
-    // If the item is not in the cart, add it.
-    cartaddRemove.style.display = 'block'; // Show the add/remove button.
-    cart.push({ id:foodId , price:foodPrice, name:foodName }); // Add the item to the cart array.
-}
-
-updateCartCount(); // Update the cart count displayed on the page.
-}
-
-// Function to update the cart item count displayed in the UI.
-
-function updateCartCount() {
-const itemNumber = $.querySelector('.itemNumber'); // Select the element showing the cart item count.
-if (cart.length === 0) {
-    // If the cart is empty, display (0).
-    itemNumber.innerHTML = `(0)`;
-} else {
-    // Otherwise, display the current number of items in the cart.
-    itemNumber.innerHTML = `(${cart.length})`;
-}
-}))*/
 
 // Loop through all cart buttons and add an event listener to handle clicks.
 
@@ -70,6 +35,7 @@ function toggleCart(button) {
     }
 
     updateCartCount(); // Update the cart count displayed on the page.
+    updateCartList();
 }
 
 // Function to update the cart item count displayed in the UI.
@@ -84,6 +50,10 @@ function updateCartCount() {
     }
 }
 
+function updateCartList() {
+const buyItem=$.querySelector('.buyItem')
+buyItem.innerHTML = cart.map((item) => `<li>${item.name}</li>`).join('');
+}
 
 // Select all buttons for adding and removing individual food items.
 const addCart = $.querySelectorAll('.addCart'); 
